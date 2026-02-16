fcf5cc2d8c1fe8e5535b4f1951cd275f0c72d1ad
wcabrera-2022060 <wcabrera-2022060@kinal.edu.gt>
2026-02-16 06:39:29 -0600

    Utilidades de expresiones

---
+++ src/main/java/utils/InfixToPostfix.java
@@ -0,0 +1,58 @@
+package utils;
+
+import ADT.Stack;
+
+public class InfixToPostfix {
+
+    public static String convert(String infix, Stack<Character> stack) {
+
+        StringBuilder postfix = new StringBuilder();
+        StringBuilder number = new StringBuilder();
+
+        for (int i = 0; i < infix.length(); i++) {
+
+            char c = infix.charAt(i);
+
+            if (Character.isDigit(c)) {
+                number.append(c);
+            } else {
+
+                if (number.length() > 0) {
+                    postfix.append(number).append(" ");
+                    number.setLength(0);
+                }
+
+                if (c == '(') {
+                    stack.push(c);
+                }
+                else if (c == ')') {
+                    while (!stack.isEmpty() && stack.peek() != '(')
+                        postfix.append(stack.pop()).append(" ");
+                    stack.pop();
+                }
+                else if (c == ' ') {
+                    continue;
+                }
+                else {
+                    while (!stack.isEmpty() && precedence(stack.peek()) >= precedence(c))
+                        postfix.append(stack.pop()).append(" ");
+                    stack.push(c);
+                }
+            }
+        }
+
+        if (number.length() > 0)
+            postfix.append(number).append(" ");
+
+        while (!stack.isEmpty())
+            postfix.append(stack.pop()).append(" ");
+
+        return postfix.toString().trim();
+    }
+
+    private static int precedence(char op) {
+        if (op == '+' || op == '-') return 1;
+        if (op == '*' || op == '/') return 2;
+        return 0;
+    }
+}
---
+++ src/main/java/utils/PostfixEvaluator.java
@@ -0,0 +1,30 @@
+package utils;
+
+import ADT.Stack;
+
+public class PostfixEvaluator {
+
+    public static int evaluate(String postfix, Stack<Integer> stack) {
+
+        String[] tokens = postfix.split(" ");
+
+        for (String token : tokens) {
+
+            if (token.matches("\\d+")) {
+                stack.push(Integer.parseInt(token));
+            } else {
+                int b = stack.pop();
+                int a = stack.pop();
+
+                switch (token) {
+                    case "+": stack.push(a + b); break;
+                    case "-": stack.push(a - b); break;
+                    case "*": stack.push(a * b); break;
+                    case "/": stack.push(a / b); break;
+                }
+            }
+        }
+
+        return stack.pop();
+    }
+}
